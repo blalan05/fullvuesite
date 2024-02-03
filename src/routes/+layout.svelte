1fee08c95e9910888c1d9cf955866bcc4c4e734e
Blaine Landowski <blalan05@gmail.com>
2024-02-02 20:59:25 -0600

    init Blaine's version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,354 +1,149 @@
-<nav class="desktop-nav">
+<script lang="ts">
+  const menuItems = [
+    { title: "Home", link: "/" },
+    { title: "Modules", link: "/modules" },
+    { title: "Industries", link: "/" },
+    { title: "Pricing", link: "/" },
+    { title: "About", link: "/" },
+    { title: "Webinar", link: "/" },
+    { title: "Contact", link: "/" },
+  ];
 
-  <div class="nav-logo-container" style="position: relative">
-    <a href="/"><img class="logo" src="{logo}" alt="FullVue logo" width="250px" height="50px"></a>
-    <h1 style="font-size: 6px; z-index: -1; display: inline; position: absolute; left: 0; top: 0; color: white;">Foundational Technologies LLC (FoundTech)</h1>
-  </div>
-
-  <div class="dt-nav-container">
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/docs">Docs</a></li>
-      <li><a href="/blog">Blog</a></li>
-      <li><a href="/pricing">Pricing</a></li>
-    </ul>
-  </div>
-
-  <div class="call-to-action-btn-dt">
-    <a href="/contact">Contact Us</a>
-  </div>
-
-</nav>
-
-<nav class:open="{isOpen}">
-  <div class="mobile-nav">
-    <button class="nav-btn" class:open="{isOpen}" on:click="{toggleNav}" on:keypress={keyboardToggle}>
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-
-    <div class="nav-logo-container">
-      <img class="logo" src={logo} alt="FullVue logo" width="250" height="50"/>
-    </div>
-
-    <div class="call-to-action-btn">
-      <a href="/contact">
-        <span class="hideDesktop">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon">
-            <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-            <path fill="white" d="M0 64H512v80L256 320 0 144V64zM0 448V182.8L237.9 346.4 256 358.8l18.1-12.5L512 182.8V448H0z"/>
-          </svg>
-        </span>
-        <span class="hideMobile">
-          Contact Us
-        </span>
-      </a>
-    </div>
-  </div>
-
-  <div class="oc-nav-container" class:open="{isOpen}">
-    <ul>
-      <li on:click={isOpen ? toggleNav : null} on:keypress={closeOnKeyPress}><a href="/">Home</a></li>
-      <li on:click={isOpen ? toggleNav : null} on:keypress={closeOnKeyPress}><a href="/docs">Docs</a></li>
-      <li on:click={isOpen ? toggleNav : null} on:keypress={closeOnKeyPress}><a href="/blog">Blog</a></li>
-      <li on:click={isOpen ? toggleNav : null} on:keypress={closeOnKeyPress}><a href="/pricing">Pricing</a></li>
-    </ul>
-  </div>
-
-</nav>
-
-<div>
-  <main on:click={isOpen ? toggleNav : null} on:keypress={closeOnKeyPress}>
-    <slot></slot>
-  </main>
-</div>
-
-<footer>
-  <div class="footer-logo-container">
-    <img class="logo" src={logo} alt="FullVue logo" />
-    <p>Copyright Foundational Technologies { yearDate }, all rights reserved.</p>
-  </div>
-
-  <div class="footer-links-container">
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/blog">Blog</a></li>
-      <li><a href="/privacy-policy">Privacy Policy</a></li>
-      <li><a href="/docs">Docs</a></li>
-      <li><a href="/pricing">Pricing</a></li>
-    </ul>
-  </div> 
-</footer>
-<script>
-import logo from '$lib/assets/FoundTech Logo.svg'
-import { onMount } from 'svelte';
-
-const yearDate = (new Date()).getFullYear()
-
-let isOpen = false
-
-const toggleNav = () => { isOpen = !isOpen }
-
-onMount(() => {		
-  window.addEventListener('resize', () => { if (window.innerWidth >= 1000) isOpen = false });
-});
-
-
-const closeOnKeyPress = (/** @type {{ key: string; }} */ event) => {
-  if (isOpen && (event.key == 'Enter' || event.key == 'Space')) {
-    isOpen = false
-  }
-}
-
-const keyboardToggle = (/** @type {{ key: string; }} */ event) => {
-  console.log(event.key)
-  if (event.key == 'Enter' || event.key == 'Space') {
-    isOpen = !isOpen
-  } 
-}
+  let menuToggle
+  let openMenu = false
+  const closeMenu = () => openMenu = false
 </script>
-<style>
-  .hideMobile {
+
+<nav class="mainNav">
+  {#each menuItems as item}
+    <li><a href={item.link} title={item.title}>{item.title}</a></li>
+  {/each}
+</nav>
+
+<nav class="mobileNav">
+  <div class="navbar">
+    <div class="container nav-container">
+      <input class="checkbox" type="checkbox" bind:checked={openMenu} name="menuToggle" id="menuToggle" bind:this={menuToggle} />
+      <div class="hamburger-lines">
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
+      </div>
+      <div class="menu-items">
+        {#each menuItems as item}
+          <li><a href={item.link} title={item.title} on:click={closeMenu}>{item.title}</a></li>
+        {/each}
+      </div>
+    </div>
+  </div>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<footer>FullVue developed by <a href="https://foundtechsolutions.com" title="Foundational Technologies LLC">FoundTech</a> Copyright &copy; {Intl.DateTimeFormat('en-us', { year: 'numeric'}).format(new Date())} </footer>
+
+<style lang="scss">
+  .mainNav {
     display: none;
   }
 
-  .icon {
-    width: 1.7em;
-    height: 1.7em;
-    vertical-align: -.125em;
-  }
-  
-  footer {
-    display: flex;
-    flex-direction: column-reverse;
-    align-items: center;
-    justify-content: center;
-    gap: 1.6rem;
-    background-image: linear-gradient(white, var(--lightblue));
-  }
-
-  footer .footer-links-container {
-    width: 90%;
-    padding: 2rem 0 1rem 0;
-  }
-
-  footer .footer-links-container ul {
+  .navbar {
     width: 100%;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 39% 0%;
-  }
+    box-shadow: 0 1px 4px rgb(146 161 176 / 15%);
+    .container {
+      margin: auto;
+      &.nav-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 62px;
+        .checkbox {
+          position: absolute;
+          display: block;
+          height: 32px;
+          width: 32px;
+          top: 20px;
+          left: 20px;
+          z-index: 5;
+          opacity: 0;
+          cursor: pointer;
+        }
+        .hamburger-lines {
+          display: block;
+          height: 26px;
+          width: 32px;
+          position: absolute;
+          top: 17px;
+          left: 20px;
+          z-index: 2;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          .line {
+            display: block;
+            height: 4px;
+            width: 100%;
+            border-radius: 10px;
+            background: #0e2431;
+            &.line1 {
+              transform-origin: 0% 0%;
+              transition: transform 0.4s ease-in-out;
+            }
+            &.line2 {
+              transition: transform 0.2s ease-in-out;
+            }
+            &.line3 {
+              transform-origin: 0% 100%;
+              transition: transform 0.4s ease-in-out;
+            }
+          }
+        }
+        .menu-items {
+          padding: 320px 0 18px 0;
+          background: white;
+          box-shadow: inset 0 0 2000px rgba(255, 255, 255, 0.5);
+          width: 100%;
+          transform: translate(-150%);
+          display: flex;
+          flex-direction: column;
+          transition: transform 0.5s ease-in-out;
+          text-align: center;
+          li {
+            font-size: 1.5rem;
+            font-weight: 500;
+            list-style: none;
+            a {
+              text-decoration: none;
+              color: #0e2431;
+              font-weight: 500;
+              font-size: 1.2rem;
+              padding: 0.7rem;
+            
+              &:hover {
+                font-weight: bold;
+              }
+            }
+          }
+        }
 
-  footer .footer-links-container li {
-    display: inline;
-    font-size: 14px;
-    color: var(--green);
-  }
+        input[type="checkbox"]:checked ~ .menu-items {
+          transform: translateX(0);
+        }
 
-  footer .footer-links-container ul a {
-    color: var(--green);
-    text-decoration: none;
-  }
-  
-  .nav-logo-container {
-    max-width: 250px;
-    margin: 7px 7px;
-  }
+        input[type="checkbox"]:checked ~ .hamburger-lines .line1 {
+          transform: rotate(45deg);
+        }
 
-  .logo {
-    width: 100%;
-    width: 250px;
-    height: 50px;
-  }
-  
-  .footer-logo-container {
-    display: flex;
-    flex-direction: column;
-    width: 90%;
-    font-size: 10px;
-    margin-bottom: 0.4rem;
-    align-items: center;
-  }
-  .footer-logo-container img {
-    margin: 0.8rem;
-    width: 67%;
-    max-width: 30em;
-  }
+        input[type="checkbox"]:checked ~ .hamburger-lines .line2 {
+          transform: scaleY(0);
+        }
 
-  .desktop-nav {
-    display: none;
-    align-items: center;
-    gap: 10%;
-    width: 100%;
-  }
-
-  .dt-nav-container {
-    margin: auto 0 auto auto;
-  }
-  
-  .desktop-nav .dt-nav-container ul {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20%;
-  }
-
-  .desktop-nav .dt-nav-container li {
-    display: inline-block;
-    font-size: 26px;
-  }
-  
-  .desktop-nav .dt-nav-container li a {
-    text-decoration: none;
-  }
-
-  .mobile-nav {
-    display: flex;
-    position: relative;
-    align-items: center;
-    justify-content: space-between;
-    margin: 0 5% 0 5%;
-    z-index: 200;
-  }
-  
-  .oc-nav-container {
-    display: flex;
-    max-height: 0;
-    height: 0;
-    overflow: hidden;
-    transition: max-height .5s ease-in-out;
-    position: fixed;
-    width: 100%;
-    text-align: center;
-    justify-content: space-evenly;
-    font-size: 24px;
-    background-color: white;
-    box-shadow: 6px 6px 7px 0 #777777;
-    z-index: 500;
-    flex-direction: column;
-    color: black;
-    opacity: 93%;
-  }
-  
-  .oc-nav-container.open {
-    max-height: 600px;
-    height: auto;
-    padding: 1rem;
-  }
-
-  .oc-nav-container li a {
-    display: inline-block;
-    width: 100%;
-    color: var(--green);
-    padding: 0.3em;
-    opacity: 1;
-    text-decoration: none;
-  }
-  
-  .nav-btn {
-    background-color: transparent;
-    border-style: none;
-    width: 36px;
-    height: 32px;
-    position: relative;
-    cursor: pointer;
-    display: inline-block;
-  }
-
-  .nav-btn span {
-    background-color: var(--green);
-    position: absolute;
-    border-radius: 2px;
-    transition: .3s cubic-bezier(.8, .5, .2, 1.4);
-    width:100%;
-    height: 4px;
-    transition-duration: 500ms
-  }
-  .nav-btn span:nth-child(1){
-    top:3px;
-    left: 0px;
-  }
-  .nav-btn span:nth-child(2){
-    top: 12px;
-    left: 0px;
-    opacity: 1;
-  }
-  .nav-btn span:nth-child(3){
-    bottom: 7px;
-    left: 0px;
-  }
-  .nav-btn.open span:nth-child(1){
-    transform: rotate(45deg);
-    top: 8px;
-  }
-  .nav-btn.open span:nth-child(2){
-    opacity:0;
-  }
-  .nav-btn.open span:nth-child(3){
-    transform: rotate(-45deg);
-    top: 8px;
-  }
-
-  .call-to-action-btn {
-    padding: 10px 15px;
-    border-radius: 7px;
-    border-style: none;
-    color: white;
-    font-size: 16px;
-    font-weight: bold;
-    background-color: var(--green);
-  }
-
-  .call-to-action-btn {
-    padding: 6px 8px;
-  }
-
-  .call-to-action-btn-dt {
-    padding: 10px 15px;
-    font-size: 16px;
-    font-weight: bold;
-    border-radius: 7px;
-    margin: auto 1.5em auto 0;
-    color: white;
-    background-color: var(--green);
-  }
-
-  .call-to-action-btn-dt:hover {
-    background-image: linear-gradient(to right, var(--green), var(--blue))
-  }
-  
-  .call-to-action-btn-dt a {
-    text-decoration: none;
-    color: white;
-  }
-  
-  @media (min-width: 1000px) {
-    .desktop-nav {
-      display: flex;
+        input[type="checkbox"]:checked ~ .hamburger-lines .line3 {
+          transform: rotate(-45deg);
+        }
+      }
     }
-
-    .mobile-nav {
-      display: none;
-    }
-
-    footer {
-      flex-direction: row;
-      height: 10em;
-    }
-
-    .footer-logo-container {
-      align-items: flex-start;
-      margin-left: 5em;
-    }
-
-    .footer-logo-container img {
-      width: 30em;
-    }
-
-    .footer-logo-container p {
-      padding-left: 1.5em;
-    }
-    
   }
 </style>
---
+++ src/routes/modules/+page.svelte
@@ -0,0 +1,48 @@
+
+<article>
+  <section>
+    <h1>FullVue</h1>
+    <h2>Small Business Toolbox</h2>
+    <h3>Full of Tools</h3>
+  </section>
+  <section>
+    <div>
+      <div>
+        <h3>Core (Jobs)</h3>
+        <p>At the base of FullVue is your partners and job management:</p>
+        <ul>
+          <li>Employees and Customers - those you've chosen to partner with to grow your business</li>
+          <li>Customer connections and locations (addresses and Lat/Lng markers)</li>
+          <li>Group Customers in Industries</li>
+          <li>Log time, materials and additional <em>extras</em></li>
+          <li>Customizable departments to visualize workflows</li>
+          <li>Cost Sheet report enlightens profit margins and profit-losses</li>
+        </ul>
+      </div>
+      <div>
+        Core Demo Video
+      </div>
+    </div>
+  </section>
+  <section>
+    <div>
+      <div>
+        <h3>Field (Off-site Service)</h3>
+        <ul>
+          <li>Log Mileage as job data</li>
+          <li>Trip records for simplified entry</li>
+          <li>Set job schedules and Communicate travel itineraries with employees</li>
+        </ul>
+      </div>
+      <div>
+        Road Demo Video
+      </div>
+    </div>
+  </section>
+  <section>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
+  </section>
+</article>
